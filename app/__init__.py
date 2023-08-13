8579eac7ff0e7bdf907f1b0b2c793d4b87dd38f7
onemanzerg <donnydeus@gmail.com>
2023-08-13 14:17:23 +0300

    fix blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -45,6 +45,9 @@ def create_app(config_class=Config):
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
 
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
