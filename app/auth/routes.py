edf4db28b35371f1fd2e22edceea8bf5762f3ca7
onemanzerg <donnydeus@gmail.com>
2023-08-13 20:50:13 +0300

    new

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -4,7 +4,7 @@ from werkzeug.urls import url_parse
 
 from app import db
 from app.auth import bp
-from app.email import send_password_reset_email
+from app.auth.email import send_password_reset_email
 from app.auth.forms import LoginForm, RegistrationForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User
 
